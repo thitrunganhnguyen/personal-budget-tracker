eecdacdf975b5c444bb86e18487a419f94deccc2
t.nguyen <t.nguyen@uni-assist.de>
2025-04-23 14:02:02 +0200

    Add UserController with POST endpoint to create users

---
+++ backend/src/main/java/com/budgetapp/backend/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.budgetapp.backend.controller;
+
+import com.budgetapp.backend.model.User;
+import com.budgetapp.backend.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/create")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
