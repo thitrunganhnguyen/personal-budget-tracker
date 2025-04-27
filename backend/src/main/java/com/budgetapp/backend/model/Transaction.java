520bb84b4e8ae54d4eb146ff6787361fbca9aef8
t.nguyen <t.nguyen@uni-assist.de>
2025-04-27 12:02:22 +0200

    Add Transaction entity

---
+++ backend/src/main/java/com/budgetapp/backend/model/Transaction.java
@@ -0,0 +1,37 @@
+package com.budgetapp.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    private String description;
+
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="category_id")
+
+
+}
