7d91924f805e3f9caa2776179d4ecaebe5531791
t.nguyen <t.nguyen@uni-assist.de>
2025-04-28 09:16:31 +0200

    Remove redundant @ResponseStatus, use GlobalExceptionHandler only

--- backend/src/main/java/com/budgetapp/backend/exception/InvalidCredentialsException.java
+++ backend/src/main/java/com/budgetapp/backend/exception/InvalidCredentialsException.java
@@ -1,10 +1,5 @@
 package com.budgetapp.backend.exception;
 
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class InvalidCredentialsException extends RuntimeException {
     public InvalidCredentialsException() {
         super("Invalid username or password");
