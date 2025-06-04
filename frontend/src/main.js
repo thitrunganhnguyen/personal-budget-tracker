9c4c1ce249fb6ebadfb34b0552b4576714fe267f
t.nguyen <t.nguyen@uni-assist.de>
2025-06-04 14:14:28 +0200

    perf: disable Vue Devtools to reduce overhead

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const app = createApp(App)
 
+
+// Deaktiviere die Vue Devtools In-App-Komponenten-Overlay
+if (window.__VUE_DEVTOOLS_GLOBAL_HOOK__) {
+  window.__VUE_DEVTOOLS_GLOBAL_HOOK__.emit = () => {};
+}
+
+
 app.use(router)
 
 app.mount('#app')
